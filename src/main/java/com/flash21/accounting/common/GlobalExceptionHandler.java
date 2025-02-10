dad232bab22566ffed825fff1433d735f3c16568
anacnoda77 <sm1108shin@naver.com>
2025-02-10 14:09:04 +0900

    feat: 예외/에러 코드 관련 초기 세팅

---
+++ src/main/java/com/flash21/accounting/common/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.flash21.accounting.common;
+
+import com.flash21.accounting.common.exception.AccountingException;
+import com.flash21.accounting.common.exception.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountingException.class)
+    public ResponseEntity<ErrorResponse> handleTalKakException(AccountingException e) {
+        return ResponseEntity.status(e.status())
+            .body(ErrorResponse.of(e.status(), e.code(), e.message()));
+    }
+
+    // Validation 과정에서 발생한 에러들을 잡아주기 위함
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ErrorResponse.of(HttpStatus.BAD_REQUEST, "000",
+                e.getBindingResult().getAllErrors().getFirst().getDefaultMessage()));
+    }
+}
---
+++ src/main/java/com/flash21/accounting/common/exception/AccountingException.java
@@ -0,0 +1,29 @@
+package com.flash21.accounting.common.exception;
+
+import com.flash21.accounting.common.exception.errorcode.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public class AccountingException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public AccountingException(ErrorCode errorCode) {
+        super(errorCode.message());
+        this.errorCode = errorCode;
+    }
+
+    public static AccountingException of(ErrorCode errorCode) {
+        return new AccountingException(errorCode);
+    }
+
+    public HttpStatus status() {
+        return errorCode.status();
+    }
+
+    public String code() {
+        return errorCode.code();
+    }
+
+    public String message() {
+        return errorCode.message();
+    }
+}
