8b26b4206b4a34715a1d15a651a1cfb015e628c8
anacnoda77 <sm1108shin@naver.com>
2025-02-12 17:15:13 +0900

    feat: OS별 첨부파일 테스트 픽스쳐 파일 경로 다르게 설정

--- src/test/java/com/flash21/accounting/fixture/file/AttachmentFileFixture.java
+++ src/test/java/com/flash21/accounting/fixture/file/AttachmentFileFixture.java
@@ -1,15 +1,30 @@
 package com.flash21.accounting.fixture.file;
 
 import com.flash21.accounting.category.domain.APINumber;
+import com.flash21.accounting.common.exception.AccountingException;
+import com.flash21.accounting.common.exception.errorcode.FileErrorCode;
 import com.flash21.accounting.file.domain.AttachmentFile;
 
 public class AttachmentFileFixture {
 
     public static AttachmentFile createDefault() {
+        String os = System.getProperty("os.name").toLowerCase();
+        String fileSource;
+        if (os.contains("mac")) {
+            fileSource =  System.getProperty("user.home") + "/accounting/files/";
+        } else if (os.contains("linux")) {
+            fileSource =  "/var/accounting/files/";
+        } else if (os.contains("windows")) {
+            fileSource =  System.getProperty("user.home") + "\\accounting\\files\\";
+        } else {
+            throw AccountingException.of(FileErrorCode.UNSUPPORTED_OS);
+        }
+
+
         return new AttachmentFile(
             1L,
             "demo",
-            System.getProperty("user.home") + "/accounting/files/demo.png",
+            fileSource + "demo.png",
             "image/png",
             APINumber.CONTRACT,
             null
