55ba3518f17ebdda5cc70f61f5a64cc205ee6f74
rlojuyeon <ejy4632@naver.com>
2025-02-12 10:58:28 +0900

    에러코드추가

--- src/main/java/com/flash21/accounting/common/exception/errorcode/DetailContractErrorCode.java
+++ src/main/java/com/flash21/accounting/common/exception/errorcode/DetailContractErrorCode.java
@@ -8,9 +8,8 @@ public enum DetailContractErrorCode implements ErrorCode {
 
     DETAIL_CONTRACT_NOT_FOUND(HttpStatus.NOT_FOUND, "DC001", "존재하지 않는 세부계약서입니다."),
     ALREADY_EXIST_DETAIL_CONTRACT(HttpStatus.BAD_REQUEST, "DC002", "이미 등록된 세부계약서입니다."),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "DC003", "필수 입력값이 누락되었습니다.");
-    //NO_PERMISSION(HttpStatus.FORBIDDEN, "DC004", "세부계약서 접근 권한이 없습니다."),
-    //AUTHENTICATION_REQUIRED(HttpStatus.UNAUTHORIZED, "DC005", "인증되지 않은 유저입니다.");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "DC003", "필수 입력값이 누락되었습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DC004", "서버 내부 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
