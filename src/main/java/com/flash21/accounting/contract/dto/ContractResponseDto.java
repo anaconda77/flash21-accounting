af0a20b35b6c1df285ac7dab7c623530946068d4
minseunghwan <minccc42710@gmail.com>
2025-02-13 15:39:51 +0900

    write test code

--- src/main/java/com/flash21/accounting/contract/dto/ContractResponseDto.java
+++ src/main/java/com/flash21/accounting/contract/dto/ContractResponseDto.java
@@ -7,6 +7,7 @@ import com.flash21.accounting.contract.entity.Status;
 import com.flash21.accounting.correspondent.domain.Correspondent;
 import com.flash21.accounting.user.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -15,9 +16,10 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ContractResponseDto {
     private Long contractId;
-    private User adminId;
+    private User admin;
     private Category category;
     private Status status;
     private ProcessStatus processStatus;
@@ -26,5 +28,24 @@ public class ContractResponseDto {
     private LocalDate contractStartDate;
     private LocalDate contractEndDate;
     private LocalDate workEndDate;
-    private Correspondent correspondentId;
+    private Correspondent correspondent;
+
+    public static ContractResponseDto of(Long contractId, User admin, Category category, Status status,
+                                         ProcessStatus processStatus, Method method, String name,
+                                         LocalDate contractStartDate, LocalDate contractEndDate,
+                                         LocalDate workEndDate, Correspondent correspondent) {
+        return ContractResponseDto.builder()
+                .contractId(contractId)
+                .admin(admin)
+                .category(category)
+                .status(status)
+                .processStatus(processStatus)
+                .method(method)
+                .name(name)
+                .contractStartDate(contractStartDate)
+                .contractEndDate(contractEndDate)
+                .workEndDate(workEndDate)
+                .correspondent(correspondent)
+                .build();
+    }
 }
