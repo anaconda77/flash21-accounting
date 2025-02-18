4816302766c83258fa5a99171ffb8d6e0ea31d35
anacnoda77 <sm1108shin@naver.com>
2025-02-18 16:44:47 +0900

    feat: 인증 필터 예외 전달 로직 추가

--- src/main/java/com/flash21/accounting/common/config/WebConfig.java
+++ src/main/java/com/flash21/accounting/common/config/WebConfig.java
@@ -3,6 +3,8 @@ package com.flash21.accounting.common.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.flash21.accounting.common.util.MultipartJackson2HttpMessageConverter;
+import java.util.Arrays;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public List<String> skipPaths() {
+        return Arrays.asList(
+            "/application/register",
+            "/login",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/h2-console/**"
+        );
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
--- src/main/java/com/flash21/accounting/common/security/filter/JWTFilter.java
+++ src/main/java/com/flash21/accounting/common/security/filter/JWTFilter.java
@@ -1,5 +1,9 @@
 package com.flash21.accounting.common.security.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flash21.accounting.common.exception.ErrorResponse;
+import com.flash21.accounting.common.exception.errorcode.AuthErrorCode;
+import com.flash21.accounting.common.exception.errorcode.ErrorCode;
 import com.flash21.accounting.common.util.jwt.JWTUtil;
 import com.flash21.accounting.common.security.details.CustomUserDetails;
 import com.flash21.accounting.user.Role;
@@ -8,20 +12,32 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
+@RequiredArgsConstructor
 public class JWTFilter extends OncePerRequestFilter {
 
     private final JWTUtil jwtUtil;
+    private final List<String> skipPaths;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
+    private final ObjectMapper objectMapper;
 
-    public JWTFilter(JWTUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getServletPath();
+        return skipPaths.stream()
+            .anyMatch(pattern -> antPathMatcher.match(pattern, path));
     }
 
     // jwt를 검증하는 filter
@@ -29,19 +45,17 @@ public class JWTFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authorization = request.getHeader("Authorization");
-
         //Authorization 헤더 검증
-        if(authorization == null || !authorization.startsWith("Bearer ")) {
-
-            filterChain.doFilter(request, response);
+        if(authorization == null) {
+            handleInvalidTokenException(response, AuthErrorCode.NOT_EXISTING_TOKEN);
             return;
         }
 
         String token = authorization.split(" ")[1];
 
-        //토큰 소멸 시간 검증
-        if(jwtUtil.isExpired(token)) {
-            filterChain.doFilter(request, response);
+        // 토큰 유효성 및 소멸 여부 검증
+        if(!authorization.startsWith("Bearer ") || !jwtUtil.isValidToken(token)) {
+            handleInvalidTokenException(response, AuthErrorCode.INVALID_ACCESS_TOKEN);
             return;
         }
 
@@ -64,6 +78,15 @@ public class JWTFilter extends OncePerRequestFilter {
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
+    }
+
+    private void handleInvalidTokenException(HttpServletResponse response, ErrorCode error) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter()
+            .write(objectMapper.writeValueAsString(
+                ErrorResponse.of(error.status(), error.code(), error.message())));
     }
 }
 
