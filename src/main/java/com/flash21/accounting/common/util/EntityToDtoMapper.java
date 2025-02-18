1549ab9fe3abb14b904f93e40d7cb8f3b0200a0d
anacnoda77 <sm1108shin@naver.com>
2025-02-18 14:28:25 +0900

    delete: 기존 Category 관련 참조 삭제 2

--- src/main/java/com/flash21/accounting/common/util/EntityToDtoMapper.java
+++ src/main/java/com/flash21/accounting/common/util/EntityToDtoMapper.java
@@ -1,8 +1,6 @@
 package com.flash21.accounting.common.util;
 
-import com.flash21.accounting.category.domain.Category;
-import com.flash21.accounting.category.dto.request.CategoryRequest;
-import com.flash21.accounting.category.dto.response.CategoryResponse;
+
 import com.flash21.accounting.correspondent.dto.response.CorrespondentResponse;
 import com.flash21.accounting.correspondent.domain.Correspondent;
 import org.mapstruct.Mapper;
@@ -12,7 +10,6 @@ import org.mapstruct.factory.Mappers;
 @Mapper(componentModel = "spring")
 public interface EntityToDtoMapper {
     EntityToDtoMapper INSTANCE = Mappers.getMapper(EntityToDtoMapper.class);
-    Category dtoToCategory(CategoryRequest categoryRequest);
 
     @Mapping(target = "correspondentId", source="correspondent.id")
     @Mapping(target = "owner.ownerId", source="owner.ownerId")
@@ -20,5 +17,4 @@ public interface EntityToDtoMapper {
     @Mapping(target = "categoryName", source="correspondentCategory.name")
     CorrespondentResponse correspondentToDto(Correspondent correspondent);
 
-    CategoryResponse categoryToDto(Category category);
 }
