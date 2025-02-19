09c84e4b4983bf4ccff09a0a5bac3e8d81622337
minseunghwan <minccc42710@gmail.com>
2025-02-19 11:41:11 +0900

    되돌림

--- src/main/java/com/flash21/accounting/contract/entity/Method.java
+++ src/main/java/com/flash21/accounting/contract/entity/Method.java
@@ -1,30 +1,7 @@
 package com.flash21.accounting.contract.entity;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-import java.util.Arrays;
-
 public enum Method {
-    GENERAL("일반계약"),
-    BID("입찰계약"),
-    OUTSOURCING("하도급계약");
-
-    private final String name;
-
-    Method(String name) {
-        this.name = name;
-    }
-
-    @JsonValue
-    public String getName() {
-        return name;
-    }
-
-    @JsonCreator
-    public static Method fromString(String value) {
-        return Arrays.stream(Method.values())
-                .filter(m -> m.getName().equals(value))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("지원되지 않는 계약 방식: " + value));
-    }
+    GENERAL, // 일반계약
+    BID, // 입찰계약
+    OUTSOURCING // 하도급계약
 }
