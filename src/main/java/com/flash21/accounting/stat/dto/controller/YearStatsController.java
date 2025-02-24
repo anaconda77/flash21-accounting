090ea68ca0fd7700b4f5bbe969dc21979bcb1d21
minseunghwan <minccc42710@gmail.com>
2025-02-24 16:04:35 +0900

    feat : YearStatsController 생성

---
+++ src/main/java/com/flash21/accounting/stat/dto/controller/YearStatsController.java
@@ -0,0 +1,39 @@
+package com.flash21.accounting.stat.controller;
+
+import com.flash21.accounting.correspondent.domain.CorrespondentCategory;
+import com.flash21.accounting.stat.dto.request.YearStatsRequestDto;
+import com.flash21.accounting.stat.dto.response.YearStatsResponseDto;
+import com.flash21.accounting.stat.service.YearStatsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/stat")
+@RequiredArgsConstructor
+public class YearStatsController {
+
+    private final YearStatsService yearStatsService;
+
+    @GetMapping("/users/{userId}/categories/{category}/years/{year}")
+    public ResponseEntity<YearStatsResponseDto> getYearStatistics(
+            @PathVariable Long userId,
+            @PathVariable CorrespondentCategory category,
+            @PathVariable Integer year
+    ) {
+        return ResponseEntity.ok(
+                yearStatsService.getYearStatistics(userId, category, year)
+        );
+    }
+
+    @PostMapping("/users/{userId}/categories/{category}/years/{year}")
+    public ResponseEntity<YearStatsResponseDto> createYearStatistics(
+            @PathVariable Long userId,
+            @PathVariable CorrespondentCategory category,
+            @PathVariable Integer year
+    ) {
+        return ResponseEntity.ok(
+                yearStatsService.createYearStatistics(userId, category, year)
+        );
+    }
+}
